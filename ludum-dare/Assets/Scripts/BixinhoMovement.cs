a5fc96e754d38746f23a867688cb71ca52e58ad2
Andre-Heiden-Gugelmin <andre_gugelmin@hotmail.com>
2020-04-18 01:54:12 -0300

    Rotação bixinho

--- ludum-dare/Assets/Scripts/BixinhoMovement.cs
+++ ludum-dare/Assets/Scripts/BixinhoMovement.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class BixinhoMovement : MonoBehaviour
 {
     private waypoints waypoints;
+
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float TurnSpeed = 8;
+
     private Rigidbody rigidbody;
+
     private int wpIndex;
+
     private float angle;
     // Start is called before the first frame update
     void Start()
@@ -23,7 +29,9 @@ public class BixinhoMovement : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, waypoints.wp[wpIndex].position, moveSpeed * Time.deltaTime);
 
         Vector3 dir = waypoints.wp[wpIndex].position - transform.position;
-        angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * TurnSpeed);
+
         rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
 
         if (Vector3.Distance(transform.position, waypoints.wp[wpIndex].position) < 0.1f){
