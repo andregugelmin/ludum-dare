46c33e3691055e16e12a8c05bb3179e0204df227
Andre-Heiden-Gugelmin <andre_gugelmin@hotmail.com>
2020-04-18 10:34:44 -0300

    Bixinho check obstacle

--- ludum-dare/Assets/Scripts/BixinhoMovement.cs
+++ ludum-dare/Assets/Scripts/BixinhoMovement.cs
@@ -8,6 +8,7 @@ public class BixinhoMovement : MonoBehaviour
 
     [SerializeField]
     private float moveSpeed;
+    private float moveSpeedValue;
     [SerializeField]
     private float TurnSpeed = 8;
 
@@ -16,23 +17,21 @@ public class BixinhoMovement : MonoBehaviour
     private int wpIndex;
 
     private float angle;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         waypoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<waypoints>();
+        moveSpeedValue = moveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, waypoints.wp[wpIndex].position, moveSpeed * Time.deltaTime);
-
         Vector3 dir = waypoints.wp[wpIndex].position - transform.position;
         float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
         angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * TurnSpeed);
-
-        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
 
         if (Vector3.Distance(transform.position, waypoints.wp[wpIndex].position) < 0.1f){
             if(wpIndex < waypoints.wp.Length - 1)
@@ -46,5 +45,19 @@ public class BixinhoMovement : MonoBehaviour
 
         }
 
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints.wp[wpIndex].position, moveSpeedValue * Time.deltaTime);
+
+        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+
+        RaycastHit hit;
+        Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(Vector3.forward)*2, Color.red);
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2.0f))
+            moveSpeedValue = 0;
+        else
+            moveSpeedValue = moveSpeed;
     }
 }
