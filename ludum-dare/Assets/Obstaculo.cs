55348462ac7c419938a579d904e5d27566524bb1
Andre-Heiden-Gugelmin <andre_gugelmin@hotmail.com>
2020-04-18 21:28:06 -0300

    smoke

---
+++ ludum-dare/Assets/Obstaculo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculo : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject smoke;
+    void OnDestroy()
+    {
+        Instantiate(smoke, transform.position, Quaternion.identity);
+    }
+}
