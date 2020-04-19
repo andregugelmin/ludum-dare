79868f79deae9f611105ed0dcf932aa3e1269c8c
Andre-Heiden-Gugelmin <andre_gugelmin@hotmail.com>
2020-04-19 16:57:09 -0300

    creditos menu

---
+++ ludum-dare/Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject MainMenu;
+    [SerializeField]
+    private GameObject CreditsMenu;
+
+    private void Start()
+    {
+        SetActive(MainMenu);
+        SetInactive(CreditsMenu);
+    }
+    public void SetActive(GameObject menu)
+    {
+        menu.SetActive(true);
+    }
+    public void SetInactive(GameObject menu)
+    {
+        menu.SetActive(false);
+    }
+
+}
